c7850f32dfd55ddd372f415d82f7e13983bb879d
Nicolas734 <nichollaslimma734@gmail.com>
2024-04-24 18:49:23 -0300

    Adicionado as novas tabelas do banco e modules

--- src/models/preprocessing_historics.py
+++ src/models/preprocessing_historics.py
@@ -3,14 +3,13 @@ from models.base_class import Base
 from sqlalchemy.sql import func
 
 
-class Preprocessing(Base):
-    __tablename__ = 'preprocessing'
+class PreprocessingHistorics(Base):
+    __tablename__ = 'preprocessing_historics'
     id = Column(Integer, primary_key=True)
     input = Column(Text, nullable=False)
     output = Column(Text, nullable=False)
     step = Column(String(255), nullable=False)
-    doc_id = Column(ForeignKey('docs.id'), nullable=False)
-    processing_time = Column(Float, nullable=False)
-    error = Column(Text, nullable=False)
-    review_id = Column(Text, nullable=False)
+    dataset_id = Column(ForeignKey('datasets.id'), nullable=False)
+    review_id = Column(ForeignKey('reviews.id'), nullable=True)
+    processing_time = Column(Integer, nullable=False)
     created_at = Column(TIMESTAMP, nullable=False, default=func.now())
