ac4b88e3aa85ba7b7143d4235eae65698af94ad4
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2024-05-17 19:28:21 -0300

    Adicionado filtro grafico radar por idade

--- src/modules/products.py
+++ src/modules/products.py
@@ -1,6 +1,9 @@
 from models.products import Products
+from models.reviewers import Reviewers
+from models.reviews import Reviews
 from fastapi import HTTPException
 from db.db import SessionLocal
+from datetime import datetime
 
 class ProductsController:
     def create_product(self, product: dict, category_id: int):
@@ -37,4 +40,43 @@ class ProductsController:
         if not product:
             raise HTTPException(status_code=404, detail="Product not found")
         return product
+
+    def get_gender_count_by_age_range(age_range: str, product_id: int):
+        try:
+            def calculate_age(birth_year: int) -> int:
+                current_year = datetime.now().year
+                age = current_year - birth_year
+                return age
+            
+            db = SessionLocal()
+            min_age, max_age = map(int, age_range.split('-'))
+            reviewers = db.query(Reviewers).join(Reviews, Reviewers.id == Reviews.reviewer_id).filter(Reviews.product_id == product_id).all()
+            print("Product ID:", product_id)
+            print("Gender  Age")
+            for reviewer in reviewers:
+                reviewer_age = calculate_age(reviewer.birth_year)
+                print(reviewer.gender, "   ", reviewer_age)
+            
+            gender_review_count = {'M': 0, 'F': 0}
+            if not reviewers:
+                # Se não houver revisores para o produto, retorne o dicionário vazio
+                return gender_review_count
+            
+            for reviewer in reviewers:
+                reviewer_age = calculate_age(reviewer.birth_year)
+                if reviewer_age >= min_age and reviewer_age <= max_age:
+                    if reviewer.gender == 'M':
+                        gender_review_count['M'] += 1
+                    elif reviewer.gender == 'F':
+                        gender_review_count['F'] += 1
+                        
+            return gender_review_count
+        except Exception as e:
+            msg = f"[ERROR] - ProductsController >> Failed to retrieve gender review count by age range: {str(e)}"
+            raise HTTPException(status_code=500, detail=msg)
+        finally:
+            db.close()
+
+
+
 
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,1 +1,2 @@
 from .preprocessing import router as preprocessing_router
+from .products import router as produts_router
