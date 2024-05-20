09e713faefb858a527815fade0a6cb5f665ab2da
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2024-05-20 20:06:22 -0300

    ajuste busca lexico

--- src/modules/training_model.py
+++ src/modules/training_model.py
@@ -26,47 +26,33 @@ class TrainingModelController:
             return {"message": "Training_model data inserted successfully"}
         finally:
             db.close()  
+
     def get_latest_training_model_with_lexico(self):
         db = SessionLocal()
         try:
             latest_training_model = db.query(Training_model).order_by(desc(Training_model.id)).first()
             if not latest_training_model:
-                raise HTTPException(status_code=404, detail="No training models found")
-            
+                return {
+                    "training_model": {},
+                    "lexico": {}
+                }
             lexico = db.query(Lexico).filter(Lexico.id == latest_training_model.lexico_id).first()
             if not lexico:
-                raise HTTPException(status_code=404, detail="Lexico not found")
-            
+                return {
+                    "training_model": {
+                        "id": latest_training_model.id,
+                        "name": latest_training_model.name,
+                        "link": latest_training_model.link,
+                        "path": latest_training_model.path
+                    },
+                    "lexico": {}
+                }
             return {
                 "training_model": {
                     "id": latest_training_model.id,
                     "name": latest_training_model.name,
                     "link": latest_training_model.link,
                     "path": latest_training_model.path
-                },
-                "lexico": {
-                    "id": lexico.id,
-                    "lexico": lexico.lexico
-                }
-            }
-        finally:
-            db.close()
-        db = SessionLocal()
-        try:
-            training_model = db.query(Training_model).filter(Training_model.id == training_model_id).first()
-            if not training_model:
-                raise HTTPException(status_code=404, detail="Training model not found")
-            
-            lexico = db.query(Lexico).filter(Lexico.id == training_model.lexico_id).first()
-            if not lexico:
-                raise HTTPException(status_code=404, detail="Lexico not found")
-            
-            return {
-                "training_model": {
-                    "id": training_model.id,
-                    "name": training_model.name,
-                    "link": training_model.link,
-                    "path": training_model.path
                 },
                 "lexico": {
                     "id": lexico.id,
